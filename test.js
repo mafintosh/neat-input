e8ba23237b7534d6cc24e33d48003941a9262cb0
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-08-05 16:45:44 +0200

    Test backspace

--- test.js
+++ test.js
@@ -20,3 +20,30 @@ test('moving cursor left and right', function (t) {
   t.is(input.cursor, cursor, 'cursor moved right')
   t.end()
 })
+
+test('backspace', function (t) {
+  var input = neatInput({ stdin: ee })
+
+  input.set('ONE')
+  var cursor = input.cursor
+  ee.emit('keypress', undefined, { name: 'backspace' })
+  t.is(input.rawLine(), 'ON', 'E is gone')
+  t.is(input.cursor, cursor - 1, 'cursor moved left')
+
+  input.set('ONE')
+  cursor = input.cursor
+  ee.emit('keypress', undefined, { name: 'left' })
+  ee.emit('keypress', undefined, { name: 'backspace' })
+  t.is(input.rawLine(), 'OE', 'N is gone')
+  t.is(input.cursor, cursor - 2, 'cursor moved left twice')
+
+  input.set('ONE')
+  ee.emit('keypress', undefined, { name: 'left' })
+  ee.emit('keypress', undefined, { name: 'left' })
+  ee.emit('keypress', undefined, { name: 'left' })
+  ee.emit('keypress', undefined, { name: 'backspace' })
+  t.is(input.rawLine(), 'ONE', 'no change')
+  t.is(input.cursor, 0, 'cursor at the start')
+
+  t.end()
+})
